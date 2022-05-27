806bc8186a888993bf2bea2bcf5ab2c2a0e71db6
Dalton Eggleston <egglestondalton@yahoo.com>
2022-05-26 21:45:14 -0700

    done

--- main.js
+++ main.js
@@ -1,28 +1,27 @@
-let continue = true
+function deafGrandma(){
+let numberOfGoodbyes = 0
 
-while ( continue ) {
+let userInput = window.prompt("HEY KID");
+while ( numberOfGoodbyes < 2 ) {
 
-    let goodbyes = 0
-    let userInput = prompt()
+   
+    
 
     if ( userInput = "" ) {
-        alert("WHAT!?")
+        userInput = window.prompt("WHAT?!")
+    }else if (userInput == "GOODBYE!" && numberOfGoodbyes == 0){
+        userInput = window.prompt("LEAVING SO SOON?")
+        numberOfGoodbyes = 1;
+    }else if (userInput == "GOODBYE!" && numberOfGoodbyes == 1){
+        userInput = window.prompt("LATER, SKATER!");
+        numberOfGoodbyes = 2;
     }
-    else if ( userinput.toUppercase != statement ) {
-        alert("SPEAK UP, KID!")
+    else if ( userinput.toUppercase() != statement ) {
+        userInput = window.prompt("SPEAK UP, KID!")
     }
-    else if ( statement.toUppercase == statement ) {
-        alert('NO, NOT SINCE 1946!')
-    }
-    else if ( userinput == "GOODBYE!" ) {
-        goodbyes + 1
-
-        if ( goodbyes == 1 ) {
-            alert("LEAVING SO SOON?')
-        }
-        else if ( goodbyes == 2 ) {
-            alert("LATER, SKATER!")
-            continue = false
-        }
+    else if ( userInput.toUppercase() == statement ) {
+        userInput = window.prompt('NO, NOT SINCE 1946!')
     }
 }
+}
+deafGrandma();
